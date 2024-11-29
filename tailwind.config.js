b57dadf2c068f84bf6fd9c070d3d6da6259727cd
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2024-11-29 14:23:53 +0700

    add more files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Укажите путь к вашим HTML и JS файлам
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
